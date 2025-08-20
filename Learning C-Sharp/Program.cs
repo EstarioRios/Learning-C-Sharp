4c1369bc4b3b7eaf2f10e7785271f1a52aea2da3
Abolfazl Khezri <abolfazl.khezri.business@gmail.com>
2025-08-20 22:18:29 +0330

    almost nothing

--- Learning C-Sharp/Program.cs
+++ Learning C-Sharp/Program.cs
@@ -1,1 +1,54 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello World!");
+        //Console.WriteLine(2 + 3); // integer
+        //Console.WriteLine("2" + "5"); // string
+        //Console.WriteLine("2 + 3 = " + 2 + 3); // completely string 
+        ///*Wellcome to the this part of coding*/
+
+
+        //string firstName = "Abolfazl";
+        //string lastName = "Khezri";
+        //int age = 17;
+        //char myChar = 'a';
+        //bool amMale = true;
+        //double myDoubleNumber = 0.13D; // I sould about 'D' from ChatGPT
+
+        //Console.Write("Hello Im " + firstName + " " + lastName + ".");
+        //Console.WriteLine("");
+        //Console.WriteLine(amMale);
+        //Console.WriteLine(age);
+        //Console.WriteLine(myDoubleNumber);
+
+
+        //const int myNumber = 10; // this variable will wont change anymore
+        //Console.WriteLine(myNumber);
+
+        //int firstNumber = 1, secondNumber = 2, thirdNumber = 3, fourthNumber = 4;
+        //Console.WriteLine(firstNumber + fourthNumber);
+
+        //string firstName = "Abolfazl", lastName = "Khezri";
+        //Console.WriteLine("My name is: " + firstName + " " + lastName);
+
+
+        //double myNum = 12.3;
+        //double myNum = 12.3D;
+        //float mySecondNum = 0.12F;
+
+        //int myNum = 341;
+        //long mySecondNum = myNum;
+        //long myThirdNum = 431;
+        //long myFourthNum = 1222222222222222232L;
+
+        //string myFirstNum = "12";
+        //int mySecondNum = Convert.ToInt32(myFirstNum);
+
+
+
+
+    }
+}
