ce8a39d2f6dff76237651db64e125d644594afab
Abolfazl Khezri <abolfazl.khezri.business@gmail.com>
2025-12-10 17:50:19 +0330

    almost nothing

--- Learning C-Sharp/Program.cs
+++ Learning C-Sharp/Program.cs
@@ -5,54 +5,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        var numbers = new List<int>
-        {
-            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-            21,22,23,24,25,26,27,28,29,30,31
-        };
+        var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("Hello");
+
 
         void CountCalculator(int goalCount, List<int> numbers)
         {
-            if (goalCount <= numbers.Count)
+            if (goalCount > numbers.Count)
             {
-                int console_time = 0;
-                foreach (var number in numbers)
-                {
-                    if (console_time < goalCount)
-                    {
-                        Console.WriteLine(number);
-                        console_time++;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-
-                if (console_time == numbers.Count)
-                    Console.WriteLine("Finished");
-                else
-                    Console.WriteLine("your Process Finished");
+                Console.WriteLine($"your entered number is bigger than {numbers.Count}");
             }
             else
             {
-                Console.WriteLine($"Your target goal is high, max is {numbers.Count}");
-                Console.WriteLine("Try one more time");
+                var countedTime = 0;
+                foreach (var item in numbers)
+                {
+                    if (countedTime != goalCount)
+                    {
+                        Console.WriteLine(item);
+                        countedTime++;
+                    }
+
+                }
+                if (countedTime == numbers.Count)
+                {
+                    Console.WriteLine("Finished");
+                }
+                else if (countedTime < numbers.Count)
+                {
+                    Console.WriteLine("Procces Finished");
+                }
             }
         }
+        ;
 
         while (true)
         {
-            Console.WriteLine($"Write a number from 1 to {numbers.Count}");
+            Console.WriteLine($"Please Enter a number betwin {numbers[0]} and {numbers.Count}");
             var input = Console.ReadLine();
-            Console.WriteLine("===============================");
-
-            if (!int.TryParse(input, out int goalCount))
-            {
-                Console.WriteLine("Not a valid number. Try again.");
-                continue;
-            }
-
+            var goalCount = Convert.ToInt16(input);
             CountCalculator(goalCount, numbers);
         }
     }
